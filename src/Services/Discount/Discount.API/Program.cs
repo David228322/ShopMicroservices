4cacc8a4de0c804d6ea6d8e92bf7420fc73a3a13
david.dzabakhidze <david.dzabakhidze@hys-enterprise.com>
2023-04-18 09:50:00 +0300

    Added Busket.API

---
+++ src/Services/Discount/Discount.API/Program.cs
@@ -0,0 +1,30 @@
+using Discount.API.Extensions;
+using Discount.API.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
+builder.Services.Configure<RouteOptions>(options =>
+{
+    options.LowercaseUrls = true;
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.MigrateDatabase<Program>();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
